3897c9dd41701f57cdf575a6d44e7fca5648c459
Christopher Toumanian <cctoumanian@gmail.com>
2019-07-05 00:56:37 -0700

    placed comments in bitmap.c

--- bitmap.c
+++ bitmap.c
@@ -2,59 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-void saveImageBitmap(char* filename, int w, int h, unsigned char image[w][h])
+void saveImageBitmap(char* filename, int w, int h, unsigned char data[w][h])
 {
   FILE *f;
-  unsigned char *img = NULL;
-  int filesize = 54 + 3 * w * h;
+  unsigned char *image = NULL;
+  int fileSize = 54 + 3 * w * h;
   int i;
-	
-  img = (unsigned char*)malloc(3 * w * h);
-  memset(img, 0, 3 * w * h);
 
+  // bitmap header values
+  unsigned char fileHeader[14] = {'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0};
+  unsigned char infoHeader[40] = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 0};
+  unsigned char pad[3] = {0, 0, 0};
+  
+  // allocates memory for image
+  image = (unsigned char*)malloc(3 * w * h);
+  memset(image, 0, 3 * w * h);
+
+  // sets single value to RGB (black & white) for each element of image
   for (i = 0; i < w; i++)
   {
     for (int j = 0; j < h; j++)
     {
       int x = i;
       int y = j;
-      int r = image[i][j];
-      int g = image[i][j];
-      int b = image[i][j];
+      int r = data[i][j];
+      int g = data[i][j];
+      int b = data[i][j];
 
-      img[(x + y * w) * 3 + 2] = (unsigned char)(r);
-      img[(x + y * w) * 3 + 1] = (unsigned char)(g);
-      img[(x + y * w) * 3 + 0] = (unsigned char)(b);
+      image[(x + y * w) * 3 + 2] = (unsigned char)(r);
+      image[(x + y * w) * 3 + 1] = (unsigned char)(g);
+      image[(x + y * w) * 3 + 0] = (unsigned char)(b);
     }
   }
 
-  unsigned char bmpfileheader[14] = {'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0};
-  unsigned char bmpinfoheader[40] = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 0};
-  unsigned char bmppad[3] = {0, 0, 0};
+  // 
+  fileHeader[2] = (unsigned char)(fileSize);
+  fileHeader[3] = (unsigned char)(fileSize >> 8);
+  fileHeader[4] = (unsigned char)(fileSize >> 16);
+  fileHeader[5] = (unsigned char)(fileSize >> 24);
+  infoHeader[4] = (unsigned char)(w);
+  infoHeader[5] = (unsigned char)(w >> 8);
+  infoHeader[6] = (unsigned char)(w >> 16);
+  infoHeader[7] = (unsigned char)(w >> 24);
+  infoHeader[8] = (unsigned char)(h);
+  infoHeader[9] = (unsigned char)(h >> 8);
+  infoHeader[10] = (unsigned char)(h >> 16);
+  infoHeader[11] = (unsigned char)(h >> 24);
 
-  bmpfileheader[2] = (unsigned char)(filesize);
-  bmpfileheader[3] = (unsigned char)(filesize >> 8);
-  bmpfileheader[4] = (unsigned char)(filesize >> 16);
-  bmpfileheader[5] = (unsigned char)(filesize >> 24);
-  bmpinfoheader[4] = (unsigned char)(w);
-  bmpinfoheader[5] = (unsigned char)(w >> 8);
-  bmpinfoheader[6] = (unsigned char)(w >> 16);
-  bmpinfoheader[7] = (unsigned char)(w >> 24);
-  bmpinfoheader[8] = (unsigned char)(h);
-  bmpinfoheader[9] = (unsigned char)(h >> 8);
-  bmpinfoheader[10] = (unsigned char)(h >> 16);
-  bmpinfoheader[11] = (unsigned char)(h >> 24);
-
+  // open or create file and write header
   f = fopen(filename, "wb");
-  fwrite(bmpfileheader, 1, 14, f);
-  fwrite(bmpinfoheader, 1, 40, f);
+  fwrite(fileHeader, 1, 14, f);
+  fwrite(infoHeader, 1, 40, f);
 
+  // write image data to file
   for (i = 0; i < h; i++)
   {
-    fwrite(img + (w * (h - i - 1) * 3), 3, w, f);
-    fwrite(bmppad, 1, (4 - (w * 3) % 4 ) % 4, f);
+    fwrite(image + (w * (h - i - 1) * 3), 3, w, f);
+    fwrite(pad, 1, (4 - (w * 3) % 4 ) % 4, f);
   }
 
-  free(img);
+  // deallocate image memory and close file
+  free(image);
   fclose(f);
 }
